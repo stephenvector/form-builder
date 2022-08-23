17218949082ee490ce42d6b041daf3200b8b98ab
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2022-08-22 21:09:07 -0400

    fix: inline firebase config values to solve CI woes

--- firebase/index.tsx
+++ firebase/index.tsx
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// These are public in the browser, so they're not
+// secret. We'll move these to environment variables
+// eventually.
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: process.env
-    .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
+  apiKey: "AIzaSyAbmCn-SQtEKq09M_G4ZQLlndoHyt_41d4",
+  authDomain: "form-builder-d15c3.firebaseapp.com",
+  projectId: "form-builder-d15c3",
+  storageBucket: "form-builder-d15c3.appspot.com",
+  messagingSenderId: "543663630959",
+  appId: "1:543663630959:web:eaefed630912192fe7bb9a",
+  measurementId: "G-9LMDXWDEG4",
 };
 
 export const app = initializeApp(firebaseConfig);
